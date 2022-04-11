9387f962dbd407443e7e37c8494d38e6e8372db6
Muhammad <101535131+jmthonar@users.noreply.github.com>
2022-04-11 18:17:44 +0300

    Add files via upload

---
+++ userbot/core/session.py
@@ -0,0 +1,45 @@
+import sys
+
+from telethon.network.connection.tcpabridged import ConnectionTcpAbridged
+from telethon.sessions import StringSession
+
+from ..Config import Config
+from .client import JmthonUserBotClient
+from pySmartDL import SmartDL
+from pytgcalls import PyTgCalls
+__version__ = "2.10.6"
+
+loop = None
+
+if Config.STRING_SESSION:
+    session = StringSession(str(Config.STRING_SESSION))
+else:
+    session = "jmthon"
+
+try:
+    jmthon = JmthonUserBotClient(
+        session=session,
+        api_id=Config.APP_ID,
+        api_hash=Config.API_HASH,
+        loop=loop,
+        app_version=__version__,
+        connection=ConnectionTcpAbridged,
+        auto_reconnect=True,
+        connection_retries=None,
+    )
+    call_py = PyTgCalls(jmthon)
+except Exception as e:
+    print(f"STRING_SESSION - {e}")
+    sys.exit()
+
+
+jmthon.tgbot = tgbot = JmthonUserBotClient(
+    session="JmthonTgbot",
+    api_id=Config.APP_ID,
+    api_hash=Config.API_HASH,
+    loop=loop,
+    app_version=__version__,
+    connection=ConnectionTcpAbridged,
+    auto_reconnect=True,
+    connection_retries=None,
+).start(bot_token=Config.TG_BOT_TOKEN)
