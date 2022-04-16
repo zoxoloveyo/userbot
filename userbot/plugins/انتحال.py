138c3a052a353149b6ac01d55c6df0600012e16a
Muhammad <101535131+jmthonar@users.noreply.github.com>
2022-04-16 18:04:42 +0300

    Create انتحال.py

---
+++ userbot/plugins/انتحال.py
@@ -0,0 +1,70 @@
+import html
+
+from telethon.tl import functions
+from telethon.tl.functions.users import GetFullUserRequest
+
+from ..Config import Config
+from ..sql_helper.globals import gvarstatus
+from . import ALIVE_NAME, BOTLOG, BOTLOG_CHATID, jmthon, edit_delete, get_user_from_event
+
+DEFAULTUSER = gvarstatus("FIRST_NAME") or ALIVE_NAME
+DEFAULTUSERBIO = (
+    gvarstatus("BIO") or "﴿ لا تَحزَن إِنَّ اللَّهَ مَعَنا ﴾"
+)
+
+
+@jmthon.ar_cmd(pattern="انتحال(?:\s|$)([\s\S]*)")
+async def _(event):
+    replied_user, error_i_a = await get_user_from_event(event)
+    if replied_user is None:
+        return
+    user_id = replied_user.id
+    profile_pic = await event.client.download_profile_photo(user_id, Config.TEMP_DIR)
+    first_name = html.escape(replied_user.first_name)
+    if first_name is not None:
+        first_name = first_name.replace("\u2060", "")
+    last_name = replied_user.last_name
+    if last_name is not None:
+        last_name = html.escape(last_name)
+        last_name = last_name.replace("\u2060", "")
+    if last_name is None:
+        last_name = "⁪⁬⁮⁮⁮⁮ ‌‌‌‌"
+    replied_user = (await event.client(GetFullUserRequest(replied_user.id))).full_user
+    user_bio = replied_user.about
+    if user_bio is not None:
+        user_bio = replied_user.about
+    await event.client(functions.account.UpdateProfileRequest(first_name=first_name))
+    await event.client(functions.account.UpdateProfileRequest(last_name=last_name))
+    await event.client(functions.account.UpdateProfileRequest(about=user_bio))
+    try:
+        pfile = await event.client.upload_file(profile_pic)
+    except Exception as e:
+        return await edit_delete(event, f"**فشل الانتحال بسبب الخطأ:**\n__{e}__")
+    await event.client(functions.photos.UploadProfilePhotoRequest(pfile))
+    await edit_delete(event, "**⪼ تم انتحال هذا المستخدم**")
+    if BOTLOG:
+        await event.client.send_message(
+            BOTLOG_CHATID,
+            f"#انتحال\n⪼ تم انتحال [{first_name}](tg://user?id={user_id })",
+        )
+
+
+@jmthon.ar_cmd(pattern="اعادة$")
+async def revert(event):
+    firstname = DEFAULTUSER
+    lastname = gvarstatus("LAST_NAME") or ""
+    bio = DEFAULTUSERBIO
+    await event.client(
+        functions.photos.DeletePhotosRequest(
+            await event.client.get_profile_photos("me", limit=1)
+        )
+    )
+    await event.client(functions.account.UpdateProfileRequest(about=bio))
+    await event.client(functions.account.UpdateProfileRequest(first_name=firstname))
+    await event.client(functions.account.UpdateProfileRequest(last_name=lastname))
+    await edit_delete(event, "**⪼ تم اعادة حسابك الى وضعه السابق**")
+    if BOTLOG:
+        await event.client.send_message(
+            BOTLOG_CHATID,
+            "#اعادة\n⪼ تم اعادة حسابك الى وضعه السابق",
+        )
