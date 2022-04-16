9d42047a9a72f13821b1d39970a7e6bca372fc7f
Muhammad <101535131+jmthonar@users.noreply.github.com>
2022-04-16 06:42:19 +0300

    Update تنويع.py

--- userbot/plugins/تنويع.py
+++ userbot/plugins/تنويع.py
@@ -1,14 +1,11 @@
-from asyncio import sleep
-from userbot import jmthon
-from geopy.geocoders import Nominatim
-from telethon.tl import types
-from userbot import jmthon
-from ..core.managers import edit_or_reply
-from ..helpers import reply_id
-from covid import Covid
 import os
 import re
 from telethon import Button
+from asyncio import sleep
+from geopy.geocoders import Nominatim
+from telethon.tl import types
+from ..core.managers import edit_or_reply
+from ..helpers import reply_id
 from ..Config import Config
 from . import jmthon, edit_delete, reply_id
 
@@ -63,36 +60,3 @@ async def _(event):
     await event.delete()
     await sleep(ttl)
     await event.respond(message)
-
-@jmthon.ar_cmd(pattern="كورونا(?:\s|$)([\s\S]*)")
-async def corona(event):
-    input_str = event.pattern_match.group(1)
-    country = (input_str).title() if input_str else "world"
-    jmthonevent = await edit_or_reply(event, "⪼ يتم سحب المعلومات")
-    covid = Covid(source="worldometers")
-    try:
-        country_data = covid.get_status_by_country_name(country)
-    except ValueError:
-        country_data = ""
-    if country_data:
-        hmm1 = country_data["confirmed"] + country_data["new_cases"]
-        hmm2 = country_data["deaths"] + country_data["new_deaths"]
-        data = ""
-        data += f"\n⪼ الاصابات المؤكده : <code>{hmm1}</code>"
-        data += f"\n⪼ الاصابات المشبوهه : <code>{country_data['active']}</code>"
-        data += f"\n⪼ الوفيات : <code>{hmm2}</code>"
-        data += f"\n⪼ الحرجه : <code>{country_data['critical']}</code>"
-        data += f"\n⪼ حالات الشفاء : <code>{country_data['recovered']}</code>"
-        data += f"\n⪼ اجمالي الاختبارات : <code>{country_data['total_tests']}</code>"
-        data += f"\n⪼ الاصابات الجديده : <code>{country_data['new_cases']}</code>"
-        data += f"\n⪼ الوفيات الجديده : <code>{country_data['new_deaths']}</code>"
-        await jmthonevent.edit(
-            "<b>⪼ معلومات كورونا لـ {}:\n{}</b>".format(country, data),
-            parse_mode="html",
-        )
-        else:
-            await edit_delete(
-                jmthonevent,
-                "**⪼ معلومات فايروس كورونا في - {} غير متوفره**".format(country),
-                5,
-            )
