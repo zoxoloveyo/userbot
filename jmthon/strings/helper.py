827d2e9ec86852eba4678fab857de2b8de5d2a17
Muhammad <101535131+jmthonar@users.noreply.github.com>
2022-04-22 06:21:18 +0300

    Create helper.py

---
+++ jmthon/strings/helper.py
@@ -0,0 +1,24 @@
+import os
+import re
+
+import requests
+
+try:
+    from pyquery import PyQuery as pq
+except ModuleNotFoundError:
+    os.system("pip3 install pyquery")
+    from pyquery import PyQuery as pq
+
+
+
+def get_download_url(link):
+    post_request = requests.post(
+        "https://www.expertsphp.com/download.php", data={"url": link}
+    )
+
+    request_content = post_request.content
+    str_request_content = str(request_content, "utf-8")
+    download_url = pq(str_request_content)("table.table-condensed")("tbody")("td")(
+        "a"
+    ).attr("href")
+    return download_url
