29527b93eb280a1d19a73e58d8e4ba7cc06da45d
Muhammad <101535131+jmthonar@users.noreply.github.com>
2022-04-22 06:36:38 +0300

    Create الذاتية.py

---
+++ userbot/plugins/الذاتية.py
@@ -0,0 +1,10 @@
+from userbot import jmthon
+
+@jmthon.ar_cmd(pattern="(جلب الصورة|ذاتية)")
+async def roz(bakar):
+  if not bakar.is_reply:
+    return await bakar.edit('**❃ يجب عليك الرد على صورة ذاتيه التدمير او صورة مؤقته**')
+  rr9r7 = await bakar.get_reply_message()
+  pic = await rr9r7.download_media()
+  await jmthon.send_file('me', pic)
+  await bakar.delete()
