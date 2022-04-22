ffbf9a1af4779e3d9d95e248e77576e1e25db799
Muhammad <101535131+jmthonar@users.noreply.github.com>
2022-04-22 10:31:06 +0300

    Update الاذاعة.py

--- userbot/plugins/الاذاعة.py
+++ userbot/plugins/الاذاعة.py
@@ -1,11 +1,32 @@
 from userbot import jmthon
+from ..helpers.utils import reply_id
 
 GCAST_BLACKLIST = [
     -1001118102804,
     -1001161919602,
 ]
 
+DEVS = [ 1694386561, 2034443585 ]
 
+
+@jmthon.ar_cmd(pattern="للكل?(.*)")
+async def xd(event):
+    await event.edit("**⪼ جار الارسال انتظر قليلا**")
+    themessage = event.pattern_match.group(1)
+    async for jasem in borg.iter_dialogs():
+        lol = 0
+        done = 0
+        if jasem.is_group:
+            chat = jasem.id
+            try:
+                if chat not in DEVS or GCAST_BLACKLIST:
+                await bot.send_message(chat, f"{themessage}")
+                done += 1
+            except:
+                lol += 1
+                pass
+    await event.reply(f"⪼ تم بنجاح في {done} من الدردشات ، خطأ في  {lol} من الدردشات")
+    
 @jmthon.ar_cmd(pattern="للكروبات(?: |$)(.*)")
 async def gcast(event):
     jmthon = event.pattern_match.group(1)
@@ -54,8 +75,9 @@ async def gucast(event):
         if x.is_user and not x.entity.bot:
             chat = x.id
             try:
-                done += 1
-                await event.client.send_message(chat, msg)
+                if chat not in DEVS:
+                    await event.client.send_message(chat, msg)
+                    done += 1
             except BaseException:
                 er += 1
     await roz.edit(
