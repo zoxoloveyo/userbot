49fb0572729a6ae5aee235386f6dd56ad07cde98
jmthonar <jmthonar@gmail.com>
2022-04-23 07:22:52 +0000

    jmthon fixed

--- userbot/plugins/الترفيه/ترفيه.py
+++ userbot/plugins/الترفيه/ترفيه.py
@@ -1,11 +1,11 @@
 from userbot import jmthon
-import random
-from userbot import jmthon, CMD_HELP
-from userbot import jmthon
+
 from ..core.managers import edit_or_reply
 from ..helpers import get_user_from_event
 
 jmthon.on(admin_cmd(pattern="نزوج(?:\s|$)([\s\S]*)"))
+
+
 async def rzfun(mention):
     user, custom = await get_user_from_event(mention)
     if not user:
