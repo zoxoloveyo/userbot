d3977d4d32725209554d205fa375e33af2112bc0
Muhammad <101535131+jmthonar@users.noreply.github.com>
2022-04-24 18:30:15 +0300

    Update الوقتي.py

--- userbot/plugins/الوقتي.py
+++ userbot/plugins/الوقتي.py
@@ -19,7 +19,7 @@ from ..helpers.utils import _format
 from ..sql_helper.globals import addgvar, delgvar, gvarstatus
 from . import edit_delete, jmthon, logging
 
-DEFAULTUSERBIO = DEFAULT_BIO or " ﴿ لا تَحزَن إِنَّ اللَّهَ مَعَنا ﴾  "
+DEFAULTUSERBIO = Config.DEFAULT_BIO or " ﴿ لا تَحزَن إِنَّ اللَّهَ مَعَنا ﴾  "
 DEFAULTUSER = AUTONAME or Config.ALIVE_NAME
 LOGS = logging.getLogger(__name__)
 CHANGE_TIME = int(gvarstatus("CHANGE_TIME")) if gvarstatus("CHANGE_TIME") else 60
