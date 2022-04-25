b5e5f9ebe234fb9fcd8cdb47d8a28557dc256d7f
Muhammad <101535131+jmthonar@users.noreply.github.com>
2022-04-25 08:13:13 +0300

    Create السورس.py

---
+++ userbot/plugins/السورس.py
@@ -0,0 +1,64 @@
+from telethon import Button, events
+
+from ..Config import Config
+from userbot import jmthon
+
+ROZ_PIC = "https://telegra.ph/file/5f6ef13851dcf0d6fc72b.jpg"
+RAZAN = Config.TG_BOT_USERNAME
+ROZ_T = (
+    f"**⌯︙بوت جمثـون يعمل بنجاح 🤍،**\n"
+    f"**   - اصدار التليثون :** `1.23.0\n`"
+    f"**   - اصدار جمثون :** `4.0.0`\n"
+    f"**   - البوت المستخدم :** `{RAZAN}`\n"
+    f"**   - اصدار البايثون :** `3.9.6\n`"
+    f"**   - المستخدم :** {mention}\n"
+)
+
+if Config.TG_BOT_USERNAME is not None and tgbot is not None:
+
+    @tgbot.on(events.InlineQuery)
+    async def inline_handler(event):
+        builder = event.builder
+        result = None
+        query = event.text
+        await bot.get_me()
+        if query.startswith("السورس") and event.query.user_id == bot.uid:
+            buttons = [
+                [
+                    Button.url("قنـاة السـورس ⚙️", "https://t.me/JMTHON"),
+                    Button.url("المطـور 👨🏼‍💻", "https://t.me/RR77R"),
+                ]
+            ]
+            if ROZ_PIC and ROZ_PIC.endswith((".jpg", ".png", "gif", "mp4")):
+                result = builder.photo(
+                    ROZ_PIC, text=ROZ_T, buttons=buttons, link_preview=False
+                )
+            elif ROZ_PIC:
+                result = builder.document(
+                    ROZ_PIC,
+                    title="JMTHON - USERBOT",
+                    text=ROZ_T,
+                    buttons=buttons,
+                    link_preview=False,
+                )
+            else:
+                result = builder.article(
+                    title="JMTHON - USERBOT",
+                    text=ROZ_T,
+                    buttons=buttons,
+                    link_preview=False,
+                )
+            await event.answer([result] if result else None)
+
+
+@jmthon.ar_cmd(pattern="السورس")
+async def repo(event):
+    RR7PP = Config.TG_BOT_USERNAME
+    if event.reply_to_msg_id:
+        await event.get_reply_message()
+    response = await bot.inline_query(RR7PP, "السورس")
+    await response[0].click(event.chat_id)
+    await event.delete()
+
+
+# edit by ~ @RR77R
