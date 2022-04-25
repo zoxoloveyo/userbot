7360680c1c271c99f0d9b5d9c68435e14482b559
jmthonar <jmthonar@gmail.com>
2022-04-25 10:48:49 +0000

    jmthon fixed

--- userbot/plugins/التخصيص.py
+++ userbot/plugins/التخصيص.py
@@ -1,5 +1,4 @@
 from urlextract import URLExtract
-from validators.url import url
 
 from userbot import jmthon
 from userbot.core.logger import logging
@@ -19,6 +18,8 @@ oldvars = {
     "PM_TEXT": "pmpermit_txt",
     "PM_BLOCK": "pmblock",
 }
+
+
 @jmthon.ar_cmd(pattern="اضف (.*)")
 async def custom_jmthonuserbot(event):
     reply = await event.get_reply_message()
@@ -26,9 +27,16 @@ async def custom_jmthonuserbot(event):
     if reply:
         text = reply.text
     if text is None:
-        return await edit_delete(event, "**⌔∮ يجب عليك الرد على النص او الرابط حسب الفار الذي تضيفه **")
+        return await edit_delete(
+            event, "**⌔∮ يجب عليك الرد على النص او الرابط حسب الفار الذي تضيفه **"
+        )
     input_str = event.pattern_match.group(1)
-    if input_str == "كليشة الحماية" or input_str == "كليشة الحمايه" or input_str == "كليشه الحماية" or input_str == "كليشه الحمايه":
+    if (
+        input_str == "كليشة الحماية"
+        or input_str == "كليشة الحمايه"
+        or input_str == "كليشه الحماية"
+        or input_str == "كليشه الحمايه"
+    ):
         addgvar("pmpermit_txt", text)
     if input_str == "كليشة الفحص" or input_str == "كليشه الفحص":
         addgvar("ALIVE_TEMPLATE", text)
@@ -42,66 +50,102 @@ async def custom_jmthonuserbot(event):
         addgvar("ALIVE_TEXT", text)
     if input_str == "عدد التحذيرات":
         addgvar("MAX_FLOOD_IN_PMS", text)
-    if input_str == "صورة الحماية" or input_str == "صورة الحمايه" or input_str == "صوره الحماية" or input_str == "صوره الحمايه":
+    if (
+        input_str == "صورة الحماية"
+        or input_str == "صورة الحمايه"
+        or input_str == "صوره الحماية"
+        or input_str == "صوره الحمايه"
+    ):
         urls = extractor.find_urls(reply.text)
         if not urls:
-            return await edit_delete(event, "**⪼ يجب عليك الرد على رابط تلجراف اولا**", 5)
+            return await edit_delete(
+                event, "**⪼ يجب عليك الرد على رابط تلجراف اولا**", 5
+            )
         text = " ".join(urls)
         addgvar("pmpermit_pic", text)
     if input_str == "صورة الفحص" or input_str == "صوره الفحص":
         urls = extractor.find_urls(reply.text)
         if not urls:
-            return await edit_delete(event, "**⪼ يجب عليك الرد على رابط تلجراف اولا**", 5)
+            return await edit_delete(
+                event, "**⪼ يجب عليك الرد على رابط تلجراف اولا**", 5
+            )
         text = " ".join(urls)
         addgvar("ALIVE_PIC", text)
     await edit_or_reply(event, f"**₰ تم بنجاح تحديث فار {input_str} بنجاح 𓆰،**")
     if BOTLOG_CHATID:
         await event.client.send_message(
             BOTLOG_CHATID,
-                    f"#وضع_فار\
+            f"#وضع_فار\
                     \n**{input_str}** تم تحديثه بنجاح في قاعده البيانات كـ:",
-                )
+        )
 
 
 @jmthon.ar_cmd(pattern="حذف (.*)")
 async def custom_jmthonuserbot(event):
     input_str = event.pattern_match.group(1)
-    if input_str == "كليشة الحماية" or input_str == "كليشة الحمايه" or input_str == "كليشه الحماية" or input_str == "كليشه الحمايه":
+    if (
+        input_str == "كليشة الحماية"
+        or input_str == "كليشة الحمايه"
+        or input_str == "كليشه الحماية"
+        or input_str == "كليشه الحمايه"
+    ):
         if gvarstatus("pmpermit_txt") is None:
-            return await edit_delete(event, "**⎙ :: عزيزي المستخدم انت لم تقوم باضافه هذا الفار اصلا**")
+            return await edit_delete(
+                event, "**⎙ :: عزيزي المستخدم انت لم تقوم باضافه هذا الفار اصلا**"
+            )
         delgvar("pmpermit_txt")
     if input_str == "كليشة الفحص" or input_str == "كليشه الفحص":
         if gvarstatus("ALIVE_TEMPLATE") is None:
-            return await edit_delete(event, "**⎙ :: عزيزي المستخدم انت لم تقوم باضافه هذا الفار اصلا**")
+            return await edit_delete(
+                event, "**⎙ :: عزيزي المستخدم انت لم تقوم باضافه هذا الفار اصلا**"
+            )
         delgvar("ALIVE_TEMPLATE")
     if input_str == "كليشة الحظر" or input_str == "كليشه الحظر":
         if gvarstatus("pmblock") is None:
-            return await edit_delete(event, "**⎙ :: عزيزي المستخدم انت لم تقوم باضافه هذا الفار اصلا**")
+            return await edit_delete(
+                event, "**⎙ :: عزيزي المستخدم انت لم تقوم باضافه هذا الفار اصلا**"
+            )
         delgvar("pmblock")
-    if input_str == "صورة الحماية" or input_str == "صورة الحمايه" or input_str == "صوره الحماية" or input_str == "صوره الحمايه":
+    if (
+        input_str == "صورة الحماية"
+        or input_str == "صورة الحمايه"
+        or input_str == "صوره الحماية"
+        or input_str == "صوره الحمايه"
+    ):
         if gvarstatus("pmpermit_pic") is None:
-            return await edit_delete(event, "**⎙ :: عزيزي المستخدم انت لم تقوم باضافه هذا الفار اصلا**")
+            return await edit_delete(
+                event, "**⎙ :: عزيزي المستخدم انت لم تقوم باضافه هذا الفار اصلا**"
+            )
         delgvar("pmpermit_pic")
     if input_str == "صورة الفحص" or input_str == "صوره الفحص":
         if gvarstatus("ALIVE_PIC") is None:
-            return await edit_delete(event, "**⎙ :: عزيزي المستخدم انت لم تقوم باضافه هذا الفار اصلا**")
+            return await edit_delete(
+                event, "**⎙ :: عزيزي المستخدم انت لم تقوم باضافه هذا الفار اصلا**"
+            )
         delgvar("ALIVE_PIC")
     if input_str == "كليشة البوت" or input_str == "كليشه البوت":
         if gvarstatus("START_TEXT") is None:
             return await edit_delete(
-                event, "**⎙ :: عزيزي المستخدم انت لم تقوم باضافه هذا الفار اصلا**")
+                event, "**⎙ :: عزيزي المستخدم انت لم تقوم باضافه هذا الفار اصلا**"
+            )
         delgvar("START_TEXT")
     if input_str == "ايموجي الفحص":
         if gvarstatus("ALIVE_EMOJI") is None:
-            return await edit_delete(event, "**⎙ :: عزيزي المستخدم انت لم تقوم باضافه هذا الفار اصلا**")
+            return await edit_delete(
+                event, "**⎙ :: عزيزي المستخدم انت لم تقوم باضافه هذا الفار اصلا**"
+            )
         delgvar("ALIVE_EMOJI")
     if input_str == "نص الفحص":
         if gvarstatus("ALIVE_TEXT") is None:
-            return await edit_delete(event, "**⎙ :: عزيزي المستخدم انت لم تقوم باضافه هذا الفار اصلا**")
+            return await edit_delete(
+                event, "**⎙ :: عزيزي المستخدم انت لم تقوم باضافه هذا الفار اصلا**"
+            )
         delgvar("ALIVE_TEXT")
     if input_str == "عدد التحذيرات":
         if gvarstatus("MAX_FLOOD_IN_PMS") is None:
-            return await edit_delete(event, "**⎙ :: عزيزي المستخدم انت لم تقوم باضافه هذا الفار اصلا**")
+            return await edit_delete(
+                event, "**⎙ :: عزيزي المستخدم انت لم تقوم باضافه هذا الفار اصلا**"
+            )
         delgvar("MAX_FLOOD_IN_PMS")
     await edit_or_reply(
         event, f"₰ هذا الفار تم حذفه بنجاح وارجاع قيمته الى القيمه الاصلية ✅"
