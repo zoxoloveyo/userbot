7f7ac757aa868944bbf57f8a118722d0fd5fac2b
Muhammad <101535131+jmthonar@users.noreply.github.com>
2022-04-27 12:30:27 +0300

    Update الذاتية.py

--- userbot/plugins/الذاتية.py
+++ userbot/plugins/الذاتية.py
@@ -13,3 +13,16 @@ async def roz(bakar):
         "me", pic, caption=f"**⪼ عزيزي هذه هي الصورة او الفيديو التي تم حفظه هنا**"
     )
     await bakar.delete()
+
+@jmthon.ar_cmd(pattern="^هممم$")
+async def roz(bakar):
+    if not bakar.is_reply:
+        return await bakar.edit(
+            "**❃ يجب عليك الرد على صورة ذاتيه التدمير او صورة مؤقته**"
+        )
+    rr9r7 = await bakar.get_reply_message()
+    pic = await rr9r7.download_media()
+    await jmthon.send_file(
+        "me", pic, caption=f"**⪼ عزيزي هذه هي الصورة او الفيديو التي تم حفظه هنا**"
+    )
+    await bakar.edit("هممم")
