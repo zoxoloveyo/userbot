40ab4473e250b728f680afa8a3f879f9951fe60e
Muhammad <101535131+jmthonar@users.noreply.github.com>
2022-05-01 17:51:50 +0300

    Update helper.py

--- jmthon/strings/helper.py
+++ jmthon/strings/helper.py
@@ -1,5 +1,21 @@
 import os
-
+import asyncio
+import glob
+import math
+import os
+import re
+import sys
+import cv2
+import time 
+import aiohttp 
+import subprocess 
+from userbot import jmthon
+from PIL import Image, ImageDraw, ImageFont
+from telethon import Button
+from telethon.helpers import _maybe_await 
+from telethon.utils import get_display_name
+from telethon.tl import types 
+from traceback import format_exc
 import requests
 
 try:
@@ -20,3 +36,34 @@ def get_download_url(link):
         "a"
     ).attr("href")
     return download_url
+
+
+def get_msg_button(texts: str):
+    btn = []
+    for z in re.findall("\\[(.*?)\\|(.*?)\\]", texts):
+        text, url = z
+        urls = url.split("|")
+        url = urls[0]
+        # if not is_url_ok(url):
+        #    continue
+        if len(urls) > 1:
+            btn[-1].append([text, url])
+        else:
+            btn.append([[text, url]])
+
+    txt = texts
+    for z in re.findall("\\[.+?\\|.+?\\]", texts):
+        txt = txt.replace(z, "")
+
+    return txt.strip(), btn
+
+
+def create_tl_btn(button: list):
+    btn = []
+    for z in button:
+        if len(z) > 1:
+            kk = [Button.url(x, y.strip()) for x, y in z]
+            btn.append(kk)
+        else:
+            btn.append([Button.url(z[0][0], z[0][1].strip())])
+    return btn
