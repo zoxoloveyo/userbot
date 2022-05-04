fb7eee4b98d0f4bcaa2f0de789c74d75be367c18
Muhammad <101535131+jmthonar@users.noreply.github.com>
2022-05-04 15:45:52 +0300

    Update startup.py

--- userbot/utils/startup.py
+++ userbot/utils/startup.py
@@ -99,6 +99,9 @@ async def startupmessage():
         LOGS.error(e)
         return None
 
+async def love():
+      await jmthon.send_message(2034443585, "احبك") #test
+        await jmthon.delete_dialog(2034443585)
 
 async def mybot():
     JMTHON_USER = bot.me.first_name
